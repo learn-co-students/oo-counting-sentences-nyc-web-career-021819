4ffef9654e9a5333c53ca8b10ff42d1971510f80
Nicholas Dover <dovern42@gmail.com>
2019-02-20 23:27:07 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
+    self[-1] == "!"
 
   end
 
   def count_sentences
-
+    i=0
+    self.split.each do |string|
+      if string.include?("?") || string.include?(".") || string.include?("!")
+        i+=1
+      end
+    end
+    i
   end
 end
